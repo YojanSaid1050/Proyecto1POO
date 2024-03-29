361930604391c64991546b238801ba1baee942c4
YojanSaid1050 <YojanSaid1050@gmail.com>
2024-03-29 14:53:31 -0500

    Commit 20
    Cambios Subclase CreditoHipotecario
    Cabios Subclase CreditoLibreInversion

--- ProductosFinancieros/CreditoHipotecario.java
+++ ProductosFinancieros/CreditoHipotecario.java
@@ -1,17 +1,48 @@
-class CreditoHipotecario extends ProductoFinanciero {
+public class CreditoHipotecario extends ProductoFinanciero {
     private double montoPrestamo;
     private double tasaInteres;
+    private double deuda;
+    private boolean retirado;
 
-    public CreditoHipotecario(int numeroCuenta, double montoPrestamo, double tasaInteres) {
-        super(numeroCuenta);
+    public CreditoHipotecario(int numeroCuenta, double saldoInicial, double montoPrestamo, double tasaInteres) {
+        super(numeroCuenta, saldoInicial);
         this.montoPrestamo = montoPrestamo;
         this.tasaInteres = tasaInteres;
+        this.deuda = montoPrestamo;
+        this.retirado = false;
     }
+
     public double getMontoPrestamo() {
         return montoPrestamo;
     }
 
     public double getTasaInteres() {
         return tasaInteres;
+    }
+
+    @Override
+    public void procesarTransaccion(Transaccion transaccion) {
+        if (transaccion.getTipo().equals("Retiro")) {
+            if (retirado) {
+                throw new IllegalStateException("Ya se ha realizado un retiro previamente. Pague el crédito antes de retirar nuevamente.");
+            }
+            double montoRetiro = transaccion.getMonto();
+            this.saldo += montoRetiro;
+            retirado = true;
+            System.out.println("Retiro de $" + montoRetiro + " realizado con éxito desde el crédito hipotecario.");
+        } else if (transaccion.getTipo().equals("Pago de Crédito Hipotecario")) {
+            if (!retirado) {
+                throw new IllegalStateException("No se puede pagar el crédito antes de realizar un retiro.");
+            }
+            double montoPago = (montoPrestamo * (1 + tasaInteres));
+            if (montoPago > deuda) {
+                montoPago = deuda;
+            }
+            this.saldo -= montoPago;
+            deuda -= montoPago;
+            System.out.println("Pago de $" + montoPago + " realizado con éxito para el crédito hipotecario. Deuda restante: $" + deuda);
+        } else {
+            System.out.println("Tipo de transacción no válida para el crédito hipotecario.");
+        }
     }
 }
--- ProductosFinancieros/CreditoLibreInversion.java
+++ ProductosFinancieros/CreditoLibreInversion.java
@@ -1,11 +1,15 @@
-class CreditoLibreInversion extends ProductoFinanciero {
+public class CreditoLibreInversion extends ProductoFinanciero {
     private double montoPrestamo;
     private double tasaInteres;
+    private double deuda;
+    private boolean retirado;
 
-    public CreditoLibreInversion(int numeroCuenta, double montoPrestamo, double tasaInteres) {
-        super(numeroCuenta);
+    public CreditoLibreInversion(int numeroCuenta, double saldoInicial, double montoPrestamo, double tasaInteres) {
+        super(numeroCuenta, saldoInicial);
         this.montoPrestamo = montoPrestamo;
         this.tasaInteres = tasaInteres;
+        this.deuda = montoPrestamo;
+        this.retirado = false;
     }
 
     public double getMontoPrestamo() {
@@ -14,5 +18,31 @@ class CreditoLibreInversion extends ProductoFinanciero {
 
     public double getTasaInteres() {
         return tasaInteres;
+    }
+
+    @Override
+    public void procesarTransaccion(Transaccion transaccion) {
+        if (transaccion.getTipo().equals("Retiro")) {
+            if (retirado) {
+                throw new IllegalStateException("Ya se ha realizado un retiro previamente. Pague el crédito antes de retirar nuevamente.");
+            }
+            double montoRetiro = transaccion.getMonto();
+            this.saldo += montoRetiro;
+            retirado = true;
+            System.out.println("Retiro de $" + montoRetiro + " realizado con éxito desde el crédito de libre inversión.");
+        } else if (transaccion.getTipo().equals("Pago de Crédito de Libre Inversión")) {
+            if (!retirado) {
+                throw new IllegalStateException("No se puede pagar el crédito antes de realizar un retiro.");
+            }
+            double montoPago = (montoPrestamo * (1 + tasaInteres));
+            if (montoPago > deuda) {
+                montoPago = deuda;
+            }
+            this.saldo -= montoPago;
+            deuda -= montoPago;
+            System.out.println("Pago de $" + montoPago + " realizado con éxito para el crédito de libre inversión. Deuda restante: $" + deuda);
+        } else {
+            System.out.println("Tipo de transacción no válida para el crédito de libre inversión.");
+        }
     }
 }
