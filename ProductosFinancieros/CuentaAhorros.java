d22a0f27b1059c38941fae6e1e848fb4a2abfa21
YojanSaid1050 <YojanSaid1050@gmail.com>
2024-04-01 19:38:08 -0500

    Commit 32
    Cambios Subclase CuentaAhorros
    Cambios Subclase CuentaCorriente

--- ProductosFinancieros/CuentaAhorros.java
+++ ProductosFinancieros/CuentaAhorros.java
@@ -1,77 +1,31 @@
 public class CuentaAhorros extends ProductoFinanciero {
-    private boolean conectadaTarjetaDebito;
     private double tasaInteres;
 
-    public CuentaAhorros(int numeroCuenta, double saldoInicial, boolean conectadaTarjetaDebito, double tasaInteres) {
+    public CuentaAhorros(int numeroCuenta, double saldoInicial, double tasaInteres) {
         super(numeroCuenta, saldoInicial);
-        this.conectadaTarjetaDebito = conectadaTarjetaDebito;
         this.tasaInteres = tasaInteres;
     }
 
-    // Getters y setters para conectadaTarjetaDebito y tasaInteres
-
-    public boolean isConectadaTarjetaDebito() {
-        return conectadaTarjetaDebito;
-    }
-    
-    public boolean tieneTarjetaDebitoConectada() {
-        return conectadaTarjetaDebito;
-    }
-
-
     public double getTasaInteres() {
-        return this.tasaInteres;
+        return tasaInteres;
     }
 
     @Override
     public void procesarTransaccion(Transaccion transaccion) {
-        if (transaccion instanceof Retiro) {
-            Retiro retiro = (Retiro) transaccion;
-            double montoRetirado = retiro.getMonto();
-            if (montoRetirado > this.saldo) {
-                throw new IllegalArgumentException("Saldo insuficiente en la cuenta de ahorros.");
+        if (transaccion instanceof Consignacion) {
+            Consignacion consignacion = (Consignacion) transaccion;
+            double montoConsignado = consignacion.getMonto();
+            this.saldo += montoConsignado;
+            System.out.println("Se ha consignado $" + montoConsignado + " en la cuenta de ahorros. Nuevo saldo: $" + this.saldo);
+        } else if (transaccion instanceof PagoCreditoHipotecario) {
+            PagoCreditoHipotecario pagoCredito = (PagoCreditoHipotecario) transaccion;
+            double montoPago = pagoCredito.getMonto();
+            if (montoPago <= this.saldo) {
+                this.saldo -= montoPago;
+                System.out.println("Se ha realizado un pago de $" + montoPago + " desde la cuenta de ahorros. Nuevo saldo: $" + this.saldo);
+            } else {
+                System.out.println("Error: Saldo insuficiente en la cuenta de ahorros para realizar el pago.");
             }
-            this.saldo -= montoRetirado;
-            System.out.println("Se ha retirado $" + montoRetirado + " del saldo de la cuenta de ahorros.");
-        } else if (transaccion.getTipo().equals("Añadir Saldo")) {
-            double montoAñadido = transaccion.getMonto();
-            this.saldo += montoAñadido;
-            System.out.println("Se ha añadido $" + montoAñadido + " al saldo de la cuenta de ahorros.");
-        } else if (transaccion.getTipo().equals("Pago de Crédito de Libre Inversión") ||
-                transaccion.getTipo().equals("Pago de Crédito Hipotecario") ||
-                transaccion.getTipo().equals("Pago de Tarjeta de Crédito") ||
-                transaccion.getTipo().equals("Pago de Seguro")) {
-            double montoPago = transaccion.getMonto();
-            if (montoPago > this.saldo) {
-                throw new IllegalArgumentException("Saldo insuficiente en la cuenta de ahorros para realizar el pago.");
-            }
-            this.saldo -= montoPago;
-            System.out.println("Se ha realizado un pago de $" + montoPago + " desde la cuenta de ahorros.");
-        } else if (transaccion.getTipo().equals("Pago de Servicio Público")) {
-            PagoServicioPublico pagoServicio = (PagoServicioPublico) transaccion;
-            double monto = pagoServicio.getMonto();
-            String tipoServicio = pagoServicio.getTipoServicioPublico();
-            String numeroReferencia = pagoServicio.getNumeroReferencia();
-
-            if (monto > this.saldo) {
-                throw new IllegalArgumentException("Saldo insuficiente en la cuenta de ahorros para pagar el servicio público.");
-            }
-
-            this.saldo -= monto;
-            System.out.println("Se ha pagado $" + monto + " del servicio público '" + tipoServicio + "' con número de referencia '" + numeroReferencia + "' desde la cuenta de ahorros.");
-        } else if (transaccion.getTipo().equals("Transferencia")) {
-            Transferencia transferencia = (Transferencia) transaccion;
-            double montoTransferencia = transferencia.getMonto();
-            int numeroCuentaDestino = transferencia.getNumeroCuentaDestino();
-            String idClienteDestino = transferencia.getIdClienteDestino();
-            String nombreClienteDestino = transferencia.getNombreClienteDestino();
-
-            if (montoTransferencia > this.saldo) {
-                throw new IllegalArgumentException("Saldo insuficiente en la cuenta de ahorros para realizar la transferencia.");
-            }
-
-            this.saldo -= montoTransferencia;
-            System.out.println("Se ha transferido $" + montoTransferencia + " a la cuenta con número " + numeroCuentaDestino + " del cliente " + nombreClienteDestino + " (ID: " + idClienteDestino + ")");
         } else {
             System.out.println("Tipo de transacción no válida para la cuenta de ahorros.");
         }
@@ -80,7 +34,8 @@ public class CuentaAhorros extends ProductoFinanciero {
     @Override
     public String toString() {
         return "Cuenta de Ahorros:\n" +
+               "Número de cuenta: " + getNumeroCuenta() + "\n" +
                "Saldo: $" + getSaldo() + "\n" +
-               "Tasa de interés: " + tasaInteres + "%";
+               "Tasa de interés: " + (tasaInteres * 100) + "%";
     }
 }
--- ProductosFinancieros/CuentaCorriente.java
+++ ProductosFinancieros/CuentaCorriente.java
@@ -10,68 +10,32 @@ public class CuentaCorriente extends ProductoFinanciero {
         return sobregiroPermitido;
     }
 
-    public double getSaldoDisponible() {
-        return this.saldo + this.sobregiroPermitido;
-    }
     @Override
     public void procesarTransaccion(Transaccion transaccion) {
-        if (transaccion instanceof Retiro) {
-            Retiro retiro = (Retiro) transaccion;
-            double montoRetirado = retiro.getMonto();
-            if (montoRetirado > this.saldo + sobregiroPermitido) {
-                throw new IllegalArgumentException("Saldo insuficiente en la cuenta corriente.");
+        if (transaccion instanceof Consignacion) {
+            Consignacion consignacion = (Consignacion) transaccion;
+            double montoConsignado = consignacion.getMonto();
+            this.saldo += montoConsignado;
+            System.out.println("Se ha consignado $" + montoConsignado + " en la cuenta corriente. Nuevo saldo: $" + this.saldo);
+        } else if (transaccion instanceof PagoCreditoHipotecario) {
+            PagoCreditoHipotecario pagoCredito = (PagoCreditoHipotecario) transaccion;
+            double montoPago = pagoCredito.getMonto();
+            if (montoPago <= (this.saldo + sobregiroPermitido)) {
+                this.saldo -= montoPago;
+                System.out.println("Se ha realizado un pago de $" + montoPago + " desde la cuenta corriente. Nuevo saldo: $" + this.saldo);
+            } else {
+                System.out.println("Error: Saldo insuficiente en la cuenta corriente para realizar el pago.");
             }
-            this.saldo -= montoRetirado;
-            System.out.println("Se ha retirado $" + montoRetirado + " del saldo de la cuenta corriente.");
-        } else if (transaccion.getTipo().equals("Añadir Saldo")) {
-            double montoAñadido = transaccion.getMonto();
-            this.saldo += montoAñadido;
-            System.out.println("Se ha añadido $" + montoAñadido + " al saldo de la cuenta corriente.");
-        } else if (transaccion.getTipo().equals("Pago de Crédito de Libre Inversión") ||
-                transaccion.getTipo().equals("Pago de Crédito Hipotecario") ||
-                transaccion.getTipo().equals("Pago de Tarjeta de Crédito") ||
-                transaccion.getTipo().equals("Pago de Seguro")) {
-            double montoPago = transaccion.getMonto();
-            if (montoPago > this.saldo) {
-                throw new IllegalArgumentException("Saldo insuficiente en la cuenta corriente para realizar el pago.");
-            }
-            this.saldo -= montoPago;
-            System.out.println("Se ha realizado un pago de $" + montoPago + " desde la cuenta corriente.");
-        } else if (transaccion.getTipo().equals("Pago de Servicio Público")) {
-            PagoServicioPublico pagoServicio = (PagoServicioPublico) transaccion;
-            double monto = pagoServicio.getMonto();
-            String tipoServicio = pagoServicio.getTipoServicioPublico();
-            String numeroReferencia = pagoServicio.getNumeroReferencia();
-            
-            if (monto > this.saldo) {
-                throw new IllegalArgumentException("Saldo insuficiente en la cuenta corriente para pagar el servicio público.");
-            }
-            
-            this.saldo -= monto;
-            System.out.println("Se ha pagado $" + monto + " del servicio público '" + tipoServicio + "' con número de referencia '" + numeroReferencia + "' desde la cuenta corriente.");
-        } else if (transaccion.getTipo().equals("Transferencia")) {
-            Transferencia transferencia = (Transferencia) transaccion;
-            double montoTransferencia = transferencia.getMonto();
-            int numeroCuentaDestino = transferencia.getNumeroCuentaDestino();
-            String idClienteDestino = transferencia.getIdClienteDestino();
-            String nombreClienteDestino = transferencia.getNombreClienteDestino();
-            
-            if (montoTransferencia > this.saldo + sobregiroPermitido) {
-                throw new IllegalArgumentException("Saldo insuficiente en la cuenta corriente para realizar la transferencia.");
-            }
-            
-            this.saldo -= montoTransferencia;
-            System.out.println("Se ha transferido $" + montoTransferencia + " a la cuenta con número " + numeroCuentaDestino + " del cliente " + nombreClienteDestino + " (ID: " + idClienteDestino + ")");
         } else {
             System.out.println("Tipo de transacción no válida para la cuenta corriente.");
         }
     }
+
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("Número de cuenta: ").append(getNumeroCuenta()).append("\n");
-        sb.append("Saldo: ").append(getSaldo()).append("\n");
-        sb.append("Sobregiro permitido: ").append(sobregiroPermitido);
-        return sb.toString();
+        return "Cuenta Corriente:\n" +
+               "Número de cuenta: " + getNumeroCuenta() + "\n" +
+               "Saldo: $" + getSaldo() + "\n" +
+               "Sobregiro permitido: $" + sobregiroPermitido;
     }
 }
