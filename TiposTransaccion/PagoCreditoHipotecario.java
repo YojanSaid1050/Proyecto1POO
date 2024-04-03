b627b4ebbbf70737dbf1271e86f2829c25ac0a2b
YojanSaid1050 <YojanSaid1050@gmail.com>
2024-04-03 00:37:01 -0500

    Commit 44
    Cambios Subclase PagoCreditoHipotecario funcional
    Cambios Subclase PagoCreditoLibreInversion funcional

--- TiposTransaccion/PagoCreditoHipotecario.java
+++ TiposTransaccion/PagoCreditoHipotecario.java
@@ -5,17 +5,19 @@ public class PagoCreditoHipotecario extends Transaccion {
 
     @Override
     public void ejecutar(ProductoFinanciero producto) throws BancoException {
-        if (producto instanceof CreditoHipotecario) {
-            CreditoHipotecario creditoHipotecario = (CreditoHipotecario) producto;
-            double deudaActual = creditoHipotecario.getDeuda();
-            double montoPago = this.getMonto();
-            if (deudaActual < montoPago) {
-                throw new BancoException("El monto a pagar excede la deuda actual del crédito hipotecario.");
-            }
-            creditoHipotecario.realizarPagoCredito(montoPago);
-            System.out.println("Pago de $" + montoPago + " realizado exitosamente en el crédito hipotecario.");
-        } else {
+        if (!(producto instanceof CreditoHipotecario)) {
             throw new BancoException("Tipo de producto financiero incorrecto para realizar el pago de crédito hipotecario.");
         }
+
+        CreditoHipotecario creditoHipotecario = (CreditoHipotecario) producto;
+        double deudaActual = creditoHipotecario.getDeuda();
+        double montoPago = this.getMonto();
+
+        if (deudaActual < montoPago) {
+            throw new BancoException("El monto a pagar excede la deuda actual del crédito hipotecario.");
+        }
+
+        creditoHipotecario.realizarPagoCredito(montoPago);
+        System.out.println("Pago de $" + montoPago + " realizado exitosamente en el crédito hipotecario.");
     }
 }
