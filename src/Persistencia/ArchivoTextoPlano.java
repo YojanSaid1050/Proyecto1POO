66e74d57d241b9a05e7aef4029e1fdb1dea6d952
YojanSaid1050 <YojanSaid1050@gmail.com>
2024-04-10 13:14:24 -0500

    Commit 50
    Cambios ArchivoTextoPlano

--- src/Persistencia/ArchivoTextoPlano.java
+++ src/Persistencia/ArchivoTextoPlano.java
@@ -5,10 +5,12 @@ import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.List;
 
 import LogicaBanco.Cliente;
+import LogicaBanco.ProductoFinanciero;
 
 public class ArchivoTextoPlano {
 
